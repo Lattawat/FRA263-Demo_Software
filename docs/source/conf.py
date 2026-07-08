ade273a264ff7fee25c1c1356e594944eadade63
Lattawat <L.Lattawat@gmail.com>
2026-07-08 11:14:19 +0700

    update rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FRA263 Software'
+copyright = '2026, Institute of Field Robotics'
+author = 'Tian'
 
 release = '0.1'
 version = '0.1.0'
